be3c29e4ddca10bfe9d97fcb83e7409db89c4862
Victor Iwegbulem <chukwuemekavictor19@gmail.com>
2022-06-30 14:56:22 -0700

    New changes2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - main block
  * Return: 0
  */
-
 int main(void)
 {
-  put("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
