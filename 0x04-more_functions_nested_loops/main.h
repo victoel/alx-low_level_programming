afb60d172511ac256095a0ba088823fbe80fd2e0
Victor Iwegbulem <chukwuemekavictor19@gmail.com>
2022-07-07 12:56:47 -0700

    main.h added

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
+void print_line(int);
+void print_diagonal(int);
