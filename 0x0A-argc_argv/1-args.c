991a6d6c7d798482433f6d250862bc66df49be68
Victor Iwegbulem <chukwuemekavictor19@gmail.com>
2022-07-22 07:43:31 -0700

    1-argc added

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%s\n", *argv);
-
+printf("%d\n", argc - 1);
 return (0);
 }
